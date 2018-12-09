e419c3f3375a119e146f69c86d630beaaf71b8a6
Shevtsov Anton <a0660576041@gmail.com>
2018-12-09 16:10:36 +0200

    added fonts

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,12 @@
 // Include gulp
 const gulp = require('gulp');
 
-// Include Our Plugins
-const jshint = require('gulp-jshint');
+// Include Plugins
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
-// const uglify = require('gulp-uglify');
-const rename = require('gulp-rename');
 
-// Lint Task
-gulp.task('lint', () => {
-  gulp.src('src/js/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
-});
 
-// Compile Our Sass
+// Compile Sass
 gulp.task('sass', () => {
   gulp.src('src/scss/*.scss')
     .pipe(sass())
@@ -30,21 +21,16 @@ gulp.task('sass_sandbox', () => {
     .pipe(gulp.dest('sandbox/'));
 });
 
-// Concatenate & Minify JS
+
 gulp.task('scripts', () => {
   gulp.src('src/js/*.js')
-    // .pipe(concat('all.js'))
-    // .pipe(gulp.dest('dist/js'))
-    // .pipe(rename('all.min.js'))
-    // .pipe(uglify())
     .pipe(gulp.dest('dist/js'));
 });
 
 // Watch Files For Changes
 gulp.task('watch', () => {
-  // gulp.watch('src/js/*.js', ['lint', 'scripts']);
   gulp.watch('src/scss/*.scss', ['sass']);
 });
 
 // Default Task
-gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
+gulp.task('default', ['watch']);
